7c25966a4ac975291b7702ed95b8c1c35a261fc9
Antoni Pstraś <antek.pstras@gmail.com>
2021-10-11 21:52:01 +0200

    Initialized seeds.rb file with movie_ids

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+movie_ids = %w[
+  tt0232500
+  tt0322259
+  tt0463985
+  tt1013752
+  tt1596343
+  tt1905041
+  tt2820852
+  tt4630562
+]
+
+movie_ids.map { |movie_id| Movie.create!(id: movie_id) }
